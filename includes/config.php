919b2df76f566efd43e9f33dfbfe2e3675c8769c
Alex <67160329+KeepGen@users.noreply.github.com>
2021-10-27 21:26:49 +0300

    ADD: Create new files and codes for Lesson 01

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ('SITE_NAME',"ShortLink");
+define ('HOST', "http://" . $_SERVER['HTTP_HOST'] . "/PHP-ShortLink");
+
+define ('DB_HOST', '127.0.0.1'); // localhost
+define ('DB_NAME', 'shortlink');
+define ('DB_USER', '');
+define ('DB_PASS', '');
